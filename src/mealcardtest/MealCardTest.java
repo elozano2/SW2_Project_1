e3a748bf697e1187f3e57f5a2269f40b67f9eeb2
mdichiara101 <57690548+mdichiara101@users.noreply.github.com>
2020-02-14 18:29:32 -0600

    Update MealCardTest.java

--- src/mealcardtest/MealCardTest.java
+++ src/mealcardtest/MealCardTest.java
@@ -1,6 +1,4 @@
-//HW3: Meal Card Test
-//Elijah Silang CS 2365
-//7-29-2019
+
 
 package mealcardtest;
 
@@ -23,7 +21,7 @@ public class MealCardTest {
         String confirm = "y";
         while(confirm.equalsIgnoreCase("y")){
             System.out.print("Following are the available functions. What would you like to do today?\n");
-            System.out.print("1. Buy food\n2. Add funds to card\n3. Add new student\n");
+            System.out.print("1. Buy food\n2. Buy item\n3. Add funds to card\n4. Add new student\n");
             int input = scan.nextInt();
             //System.out.print("Press Enter to continue...");
             scan.nextLine();
@@ -60,15 +58,34 @@ public class MealCardTest {
                     f3.displayMenuLou(customer);
                 }
             }
-            else if(input == 2){
+           else if(input == 2){
+                System.out.print("----------------------\n");
+                System.out.print("Press Enter to continue...");
+                scan.nextLine();
+                System.out.print("Welcome to the SUB Barnes and Noble. Please enter your name to identify who will be making the purchase: ");
+                String name = scan.nextLine();
+                System.out.print("----------------------\n");
+                Student customer = new Student();
+                customer = s.getStudent(name);
+                k.display_Menu(customer);
+            }
+           else if(input == 3){
                 System.out.print("Please enter your name to identify whose account the funds will be added to: ");
                 String name = scan.nextLine(); 
                 Student customer = new Student();
                 customer = s.getStudent(name);
-                m.cardAdd(customer);
+               System.out.print("1.MealCard Deposit\n2.RaiderCash Deposit\n ");
+                int num = scan.nextInt(); 
+                if(num ==1){
+                m.cardAdd(customer);}
+                else if(num==2)
+                {
+                m.AddRaiderCash(customer);
+                }
+                scan.nextLine();
             }
         
-            else if(input == 3){
+            else if(input == 4){
                 s.newStudent();
             }
             

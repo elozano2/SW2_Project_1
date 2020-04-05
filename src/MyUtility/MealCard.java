952f7f439a9aeaf5cbb6f59f6a9895b60ea652fd
wtomah1 <62449106+wtomah1@users.noreply.github.com>
2020-04-05 11:09:40 -0500

    Update MealCard.java

--- src/MyUtility/MealCard.java
+++ src/MyUtility/MealCard.java
@@ -8,6 +8,7 @@ public class MealCard {
     public int balance;
     public double rc balance;
     public void cardIssue(Student student){
+        //Choosing a meal plan
         System.out.print("Would you like to specify a meal plan amount and RaiderCash amount? (Y/N)\n");
         String input = scan.nextLine();
         if(input.equalsIgnoreCase("y")){
@@ -19,6 +20,7 @@ public class MealCard {
             System.out.print(student.card.rcbalance + " points have been given to " + student.name + ".\n");
        
         }
+        //Setting automatic amount for the student's balance
         else if(input.equalsIgnoreCase("n")){
             student.card.balance = 100;
             student.card.rcbalance = 100;
@@ -27,17 +29,20 @@ public class MealCard {
             System.out.print("Press Enter to continue...");
         }
         else{
+            //prints out error
             System.out.print("Invalid input. Please try again.\n");
             cardIssue(student);
         }
         scan.nextLine();
     }
     public void cardAdd(Student student){
+        //Adds points to student's account
         System.out.print("How many points would you like to add to your account?\n");
         int balanceAdd = scan.nextInt();
         student.card.balance += balanceAdd;
         System.out.print(student.name + "'s balance is now " + student.card.balance + ".\n");
     }
+    //Adds Raider Cash to student's account
     public void AddRaiderCash(Student student){
         System.out.print("How much RaiderCash would you like to add to your account?\n");
         int balanceAdd = scan.nextInt();
